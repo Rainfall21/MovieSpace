a59a9fe73f04ed9d30786ea094ac7dd900785b68
alibekismagulov <na.ismagulov@gmail.com>
2023-07-07 15:19:42 +0300

    Initial Commit

---
+++ MovieSpace/MovieSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSpaceApp.swift
+//  MovieSpace
+//
+//  Created by Alibek Ismagulov on 07.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
